a5e5cbf982667af3e070c1ce38ba5a5e59c84ef6
Sho Kaneko <kanekoshoyu@gmail.com>
2025-09-03 18:12:05 +0200

    astro test SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,15 @@ import alpinejs from '@astrojs/alpinejs';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
+
 
 // https://astro.build/config
 export default defineConfig({
+  output: "static", // if we want server side rendering, add `prerender = false` at the top of the astro file
   site: 'https://bounteer.com',
+  adapter: node({ mode: "standalone" }),
+
   vite: {
     plugins: [tailwindcss()],
   },
